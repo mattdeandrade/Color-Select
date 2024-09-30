ad8add7161bb1e12538d3f80c1815f6e1f717c0d
Matthew De Andrade <fayrfax@gmail.com>
2024-09-30 14:56:06 -0400

    added highlighter button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import { useState } from "react";
 // Write your Color component here
-
-const Color = ({ color }) => {
-  return <div className={color}></div>;
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  return (
+    <div
+      className={`${color} ${selectedColor === color ? "selected" : ""}`}
+      onClick={() => {
+        setSelectedColor(color);
+      }}
+    ></div>
+  );
 };
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="blue" />
-        <Color color="black" />
-        <Color color="yellow" />
+        <Color
+          color="yellow"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="green"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="black"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
       </div>
     </div>
   );
